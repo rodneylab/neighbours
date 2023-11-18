889db3806ca3dc2949e2c93830a0d614aa0cdbc8
Rodney Lab <codesign@rodneylab.com>
2023-11-18 12:42:47 +0000

    fix: 💫 add logic for finding points close to a chosen point

--- src/domain/point.rs
+++ src/domain/point.rs
@@ -3,7 +3,7 @@ use serde::Deserialize;
 use std::{fs::read_to_string, path::Path};
 
 /// Represents direction faced by a point
-#[derive(Debug, Deserialize, PartialEq)]
+#[derive(Copy, Clone, Debug, Deserialize, PartialEq)]
 pub enum Direction {
     North,
     East,
@@ -12,7 +12,7 @@ pub enum Direction {
 }
 
 /// Represents a point as used internally
-#[derive(Debug, PartialEq)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Point {
     /// x,y coordinates of the point
     pub coordinates: (i32, i32),
@@ -35,7 +35,8 @@ pub struct PointList {
     pub points: Vec<InputPoint>,
 }
 
-/// Helper function for parsing a JSON file of points into a [`Vec`] of [`Point`]s
+/// Helper function for parsing a JSON file of points into a [`Vec`] of
+/// [`Point`]s
 pub fn parse_points_file<P: AsRef<Path>>(path: P) -> Result<Vec<Point>, AppError> {
     let path_ref = path.as_ref();
     let json = match read_to_string(path_ref) {
@@ -70,11 +71,130 @@ pub fn parse_points_file<P: AsRef<Path>>(path: P) -> Result<Vec<Point>, AppError
     Ok(result)
 }
 
+/// Distance between two points
+fn euclidean_distance((x_1, y_1): (i32, i32), (x_2, y_2): (i32, i32)) -> f64 {
+    let horizontal_distance: f64 = (x_2 - x_1).into();
+    let vertical_distance: f64 = (y_2 - y_1).into();
+    ((horizontal_distance * horizontal_distance) + (vertical_distance * vertical_distance)).sqrt()
+}
+
+/// Return a vector of all points within `radius` units of `point`.  `point` is
+/// never included in the returned vector.
+fn close_neighbours<'a>(
+    point: &'a Point,
+    radius: u32,
+    neighbourhood: &'a [Point],
+) -> Vec<&'a Point> {
+    let Point {
+        number: point_number,
+        coordinates: point_coordinates,
+        ..
+    } = point;
+    let result: Vec<&Point> = neighbourhood.iter().fold(vec![], |mut acc, val| {
+        let Point {
+            number: neighbour_number,
+            coordinates: neighbour_coordinates,
+            ..
+        } = val;
+        if point_number != neighbour_number {
+            let distance = euclidean_distance(*point_coordinates, *neighbour_coordinates);
+            if distance < radius as f64 {
+                acc.push(val);
+            }
+        }
+        acc
+    });
+    result
+}
+
+/// Return a vector of all points within `arc_radius` units of a given starting
+/// point.  The starting point is identified by `point_number`, and the
+/// universe of all points is passed as the neighbourhood.  An empty vector is
+/// returned if no point matching `point_number` is found in neighbourhood.
+/// The starting point is never included in the returned vector.
+pub fn visible_points_from_neighbours(
+    point_number: u32,
+    _arc_central_angle: u32,
+    arc_radius: u32,
+    neighbourhood: &[Point],
+) -> Vec<&Point> {
+    match neighbourhood
+        .iter()
+        .find(|Point { number, .. }| *number == point_number)
+    {
+        Some(value) => close_neighbours(value, arc_radius, neighbourhood),
+        None => vec![],
+    }
+}
+
+/// Return a vector of all points within arc_radius units of a given starting
+/// point.  The starting point is identified by `point_number`, and the
+/// universe of all points is read from `./points.json`.  An empty vector is
+/// returned if no point matching `point_number` is found in neighbourhood.
+/// The starting point is never included in the returned vector.
+pub fn visible_points(
+    point_number: u32,
+    arc_central_angle: u32,
+    arc_radius: u32,
+) -> Result<Vec<Point>, AppError> {
+    let points_file_path = Path::new("./points.json");
+    let points = match parse_points_file(points_file_path) {
+        Ok(value) => value,
+        Err(error) => {
+            eprintln!("{error}");
+            return Err(error);
+        }
+    };
+
+    let result: Vec<Point> =
+        visible_points_from_neighbours(point_number, arc_central_angle, arc_radius, &points)
+            .iter()
+            .map(|val| **val)
+            .collect();
+    Ok(result)
+}
+
 #[cfg(test)]
 mod tests {
-    use super::{parse_points_file, Direction, Point};
+    use super::{
+        euclidean_distance, parse_points_file, visible_points, visible_points_from_neighbours,
+        Direction, Point,
+    };
     use crate::utilities::AppError;
     use std::path::Path;
+
+    #[test]
+    fn euclidean_distance_gives_expected_result() {
+        // arrange
+        let point_1 = (1, 1);
+        let point_2 = (4, 5);
+
+        // act
+        let distance = euclidean_distance(point_1, point_2);
+
+        // assert
+        assert_eq!(distance, 5.0);
+
+        // arrange
+        let point_1 = (-1, -1);
+        let point_2 = (-2, -2);
+
+        // act
+        let distance = euclidean_distance(point_1, point_2);
+
+        // assert
+        assert_eq!(distance, 2.0_f64.sqrt());
+
+        // arrange
+        let point_1 = (0, 0);
+        let point_2 = (0, 0);
+
+        // act
+        let distance = euclidean_distance(point_1, point_2);
+
+        // assert
+        assert_eq!(distance, 0.0);
+    }
 
     #[test]
     fn parses_valid_points_file() -> Result<(), AppError> {
@@ -125,5 +245,70 @@ mod tests {
         assert_eq!(
             outcome,"Error reading input file: `./fixtures/does-not-exist.json`. Check it exists and contains valid UTF-8."
         );
+    }
+
+    #[test]
+    fn visible_points_handles_valid_input() -> Result<(), AppError> {
+        // arrange
+
+        // act
+        let outcome = visible_points(1, 45, 20)?;
+
+        // assert
+        assert_eq!(outcome.len(), 10);
+        Ok(())
+    }
+
+    #[test]
+    fn visible_points_from_neighbours_handles_valid_input() {
+        // arrange
+        let points: Vec<Point> = vec![
+            Point {
+                coordinates: (8, 6),
+                number: 5,
+                direction: Direction::North,
+            },
+            Point {
+                coordinates: (6, 19),
+                number: 6,
+                direction: Direction::East,
+            },
+            Point {
+                coordinates: (28, 26),
+                number: 19,
+                direction: Direction::South,
+            },
+            Point {
+                coordinates: (2, 12),
+                number: 20,
+                direction: Direction::West,
+            },
+        ];
+
+        // act
+        let outcome = visible_points_from_neighbours(20, 45, 10, &points);
+
+        // assert
+        assert_eq!(outcome.len(), 2);
+        assert!(outcome
+            .iter()
+            .find(|Point { number, .. }| *number == 5)
+            .is_some());
+        assert!(outcome
+            .iter()
+            .find(|Point { number, .. }| *number == 6)
+            .is_some());
+    }
+
+    #[test]
+    fn visible_points_from_neighbours_handles_empty_input_universe() {
+        // arrange
+        let points: Vec<Point> = vec![];
+
+        // act
+        let outcome = visible_points_from_neighbours(20, 45, 10, &points);
+
+        // assert
+        assert_eq!(outcome.len(), 0);
     }
 }
